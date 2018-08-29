315d25dba52c5e5e117469a19153dc563e37ca04
CreativeGP <malicktafaminecraft@gmail.com>
2018-08-29 22:43:35 +0900

    エラー出力をする

--- client/lib.js
+++ client/lib.js
@@ -7,71 +7,73 @@
 'use strict';
 
 class Stream {
-	constructor(sender, receiver, distributor, ready_callback) {
-		this.es = new EventSource(sender);
+    constructor(sender, receiver, distributor, ready_callback) {
+	this.es = new EventSource(sender);
 
-		this.receiver = receiver;
-		this.distributor = distributor;
+	this.receiver = receiver;
+	this.distributor = distributor;
 
-		this.Base64 = {
-			encode: function (str) {
-				return btoa(unescape(encodeURIComponent(str)));
-			},
-			decode: function (str) {
-				return decodeURIComponent(escape(atob(str)));
-			}
-		};
+	this.Base64 = {
+	    encode: function (str) {
+		return btoa(unescape(encodeURIComponent(str)));
+	    },
+	    decode: function (str) {
+		return decodeURIComponent(escape(atob(str)));
+	    }
+	};
 
-		this.taginfo = {};
+	this.taginfo = {};
 
-		{
-			let self = this;
-			this.es.addEventListener('server', function (e) {
-				self.id = e.data;
-				ready_callback();
-			}, false);
+	{
+	    let self = this;
+	    this.es.addEventListener('server', function (e) {
+		self.id = e.data;
+		ready_callback();
+	    }, false);
+	}
+    }
+
+    send(data, tags) {
+	//const tags = tags.join(',');
+	fetch(`${this.receiver}?id=${this.id}&d=${data}&t=${tags}`);
+    }
+
+    ask_tag(tags, pass) {
+	let cloj = function (self, tags, pass, callback) {
+	    return function () {
+		return callback(this.responseText, self, tags, pass);
+	    };
+	};
+	let onload = function (text, self, atags, apass) {
+	    if (text == "") {
+		const tags = atags.split(',');
+		const pass = apass.split(',');
+		for (let i = 0, len = tags.length; i < len; ++i) {
+		    self.taginfo[tags[i]] = pass[i];
 		}
-	}
+	    } else {
+                self.onError("");
+            }
+	};
 
-	send(data, tags) {
-		//const tags = tags.join(',');
-		fetch(`${this.receiver}?id=${this.id}&d=${data}&t=${tags}`);
-	}
+	let xhr = new XMLHttpRequest();
+	xhr.addEventListener('load', cloj(this, tags, pass, onload));
+	xhr.open('GET', `${this.distributor}?q=join&id=${this.id}&t=${tags}&p=${pass}`);
+	xhr.send();
+    }
 
-	ask_tag(tags, pass) {
-		let cloj = function (self, tags, pass, callback) {
-			return function () {
-				return callback(this.responseText, self, tags, pass);
-			};
-		};
-		let onload = function (text, self, atags, apass) {
-			if (text == "") {
-				const tags = atags.split(',');
-				const pass = apass.split(',');
-				for (let i = 0, len = tags.length; i < len; ++i) {
-					self.taginfo[tags[i]] = pass[i];
-				}
-			}
-		};
+    add_tag(tags, pass) {
+	fetch(`${this.distributor}?q=add&id=${this.id}&t=${tags}&p=${pass}`);
+    }
 
-		let xhr = new XMLHttpRequest();
-		xhr.addEventListener('load', cloj(this, tags, pass, onload));
-		xhr.open('GET', `${this.distributor}?q=join&id=${this.id}&t=${tags}&p=${pass}`);
-		xhr.send();
-	}
+    set OnMessage(func) {
+	// Decode newlines and call `func`
+	this.onMessage = e => func(e, e.data.split(' ')[0], e.data.substr(e.data.split(' ')[0].length+1).replace(/%\\n/g, "\n"));
+	this.es.addEventListener('d', this.onMessage);
+    }
 
-	add_tag(tags, pass) {
-		fetch(`${this.distributor}?q=add&id=${this.id}&t=${tags}&p=${pass}`);
-	}
-
-	set OnMessage(func) {
-		// Decode newlines and call `func`
-		this.onMessage = e => func(e, e.data.split(' ')[0], e.data.substr(e.data.split(' ')[0].length+1).replace(/%\\n/g, "\n"));
-		this.es.addEventListener('d', this.onMessage);
-	}
-
-	set OnError(func) {
-		this.onError = func;
-		this.es.onerror = this.onError;
-	}
+    set OnError(func) {
+	this.onError = func;
+	this.es.onerror = this.onError;
+    }
 }
